a272ff210db1b80f09ea365653239d07aa8218b2
Marcel Grummet <marcelgrummet@gmail.com>
2022-04-02 12:54:12 +0200

    updated caching with web.dev example code

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
 // This code executes in its own worker or thread
 const urlsToCache = [
-   "./index.html",
-   "./assets/js/books.js",
-   "./assets/js/popup.js",
-   "./assets/js/index.js",
-   "./assets/js/popout.js",
-   "./assets/css/index.css",
-   "./assets/css/ui/colors.css",
-   "./assets/css/ui/popup.css",
-   "./assets/css/ui/checkbox.css",
-   "./assets/css/ui/textbox.css",
-   "./assets/css/ui/header.css",
-   "./assets/css/ui/button.css",
-   "./assets/css/ui/ui.css",
-   "./assets/css/ui/popout.css",
-   "./assets/icons/scalable.svg",
-   "./assets/icons/512.png",
-   "./assets/icons/1024.png",
-   "./assets/icons/384.png",
-   "./assets/icons/180.png",
-   "./assets/icons/192.png",
-   "./assets/images/placeholder.png",
-   "./README.md"
+   "/index.html",
+   "/assets/js/books.js",
+   "/assets/js/popup.js",
+   "/assets/js/index.js",
+   "/assets/js/popout.js",
+   "/assets/css/index.css",
+   "/assets/css/ui/colors.css",
+   "/assets/css/ui/popup.css",
+   "/assets/css/ui/checkbox.css",
+   "/assets/css/ui/textbox.css",
+   "/assets/css/ui/header.css",
+   "/assets/css/ui/button.css",
+   "/assets/css/ui/ui.css",
+   "/assets/css/ui/popout.css",
+   "/assets/icons/scalable.svg",
+   "/assets/icons/512.png",
+   "/assets/icons/1024.png",
+   "/assets/icons/384.png",
+   "/assets/icons/180.png",
+   "/assets/icons/192.png",
+   "/assets/images/placeholder.png",
+   "/README.md"
  ]
 
 self.addEventListener("install", event => {
@@ -35,15 +35,37 @@ self.addEventListener("activate", event => {
 });
 
 
-self.addEventListener("fetch", e => {
-   e.respondWith(
-      caches.match(e.request).then(res => {
-         const nres = fetch(e.request).then(response => {
-            caches.open("pwa-assets").then(cache => {
-               cache.put(e.request, response.clone())
-            })
-         })
-         return res || nres
-      })
+// self.addEventListener("fetch", e => {
+//    e.respondWith(
+//       caches.match(e.request).then(res => {
+//          const nres = fetch(e.request).then(response => {
+//             caches.open("pwa-assets").then(cache => {
+//                cache.put(e.request, response.clone())
+//             })
+//             return res || nres
+//          })
+//       })
+//    )
+// })
+
+// self.addEventListener("fetch", e => {
+//    e.respondWith(
+//       fetch(e.request)
+//    )
+// })
+
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request).then(cachedResponse => {
+         const networkFetch = fetch(event.request).then(response => {
+           // update the cache with a clone of the network response
+           caches.open("pwa-assets").then(cache => {
+               cache.put(event.request, response.clone());
+           });
+         });
+         // prioritize cached response over network
+         return cachedResponse || networkFetch;
+     }
    )
-})
+  )
+});
